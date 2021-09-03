5c6fbf5fd838091d47d9abe8aae4dcf5093c0255
ChiaraBi <chiara.bigarella1@gmail.com>
2021-09-03 09:18:26 +0200

    reorganize image folder

--- src/zahng.py
+++ src/zahng.py
@@ -7,7 +7,15 @@ from colorization.colorizers.util import load_img, preprocess_img, postprocess_t
 colorizer_eccv16 = colorizers.eccv16().eval()
 # colorizer_eccv16.cuda() # uncomment this if you're using GPU
 
-img_path = '../img/ansel_adams3.jpeg'
+# images' paths
+imageNet_input_dir = '../img/original/ImageNet/'
+bnw_input_dir = '../img/original/BlackAndWhite/'
+
+imageNet_output_dir = '../img/colorized/ImageNet/'
+bnw_output_dir = '../img/colorized/BlackAndWhite/'
+
+# load and preprocess images
+img_path = bnw_input_dir + 'ansel_adams3.jpeg'
 img = load_img(img_path)
 (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
 # tens_l_rs = tens_l_rs.cuda() # uncomment this if you're using GPU
@@ -17,5 +25,5 @@ img = load_img(img_path)
 img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig, 0*tens_l_orig), dim=1))
 out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
 
-out_path = '../img/ansel_adams3-color.jpeg'
+out_path = bnw_output_dir + 'ansel_adams3-color.jpeg'
 plt.imsave(out_path, out_img_eccv16)
