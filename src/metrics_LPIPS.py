67a1f2de1bb89a47edfd93a03b583871f1ad10b5
Silvia Poletti <silvia.poletti@studenti.unipd.it>
2022-01-02 11:05:15 +0100

    metriche

--- src/metrics_LPIPS.py
+++ src/metrics_LPIPS.py
@@ -10,8 +10,10 @@ from utils_alexnet import *
 model = 'chromagan'
 
 '''
-path = '../img/colorized/'+model+'/test/'
-# path = '../img/original/test/'
+if model == 'original':
+    path = '../img/original/test/'
+else:
+    path = '../img/colorized/'+model+'/test/'
 
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
@@ -27,17 +29,14 @@ for files in onlyfiles:
 # Normalization in range [-1,1]
 data_metrics_norm = np.empty(data_metrics.shape)
 for i in range(0, data_metrics.shape[0]):
-    data_metrics_norm[i, :, :, 0] = 2*(data_metrics[i, :, :, 0] - data_metrics[i, :, :, 0].min()) / (data_metrics[i, :, :, 0].max() - data_metrics[i, :, :, 0].min()) - 1
-    data_metrics_norm[i, :, :, 1] = 2*(data_metrics[i, :, :, 1] - data_metrics[i, :, :, 1].min()) / (data_metrics[i, :, :, 1].max() - data_metrics[i, :, :, 1].min()) - 1
-    data_metrics_norm[i, :, :, 2] = 2*(data_metrics[i, :, :, 2] - data_metrics[i, :, :, 2].min()) / (data_metrics[i, :, :, 2].max() - data_metrics[i, :, :, 2].min()) - 1
-
+    data_metrics_norm[i, :, :, 0] = 2*(data_metrics[i, :, :, 0] - data_metrics[i, :, :, :].min()) / (data_metrics[i, :, :, :].max() - data_metrics[i, :, :, :].min()) - 1
+    data_metrics_norm[i, :, :, 1] = 2*(data_metrics[i, :, :, 1] - data_metrics[i, :, :, :].min()) / (data_metrics[i, :, :, :].max() - data_metrics[i, :, :, :].min()) - 1
+    data_metrics_norm[i, :, :, 2] = 2*(data_metrics[i, :, :, 2] - data_metrics[i, :, :, :].min()) / (data_metrics[i, :, :, :].max() - data_metrics[i, :, :, :].min()) - 1
 # NxHxWx3 -> Nx3xHxW
 data_metrics_norm = data_metrics_norm.transpose((0, 3, 1, 2))
 np.save('../resources/data_metrics_'+model, data_metrics_norm)
-# np.save('../resources/data_metrics_original', data_metrics_norm)
 '''
 
-'''
 originals = np.load('../resources/data_metrics_original.npy')
 colorized = np.load('../resources/data_metrics_'+model+'.npy')
 
@@ -53,13 +52,14 @@ d_alex = loss_fn_alex(originals, colorized)   # type: toarch.FloatTensor
 # d_vgg = loss_fn_vgg(originals, colorized)
 
 torch.save(d_alex, '../resources/metrics_alex_'+model+'.pt')
-# torch.save(d_vgg, '../resources/metrics_vgg_'+model)
 
+# torch.save(d_vgg, '../resources/metrics_vgg_'+model)
 # torch.load('file.pt')
-'''
 
 m = torch.load('../resources/metrics_alex_'+model+'.pt')
-metrics = dict(max=m.max().item(), min=m.min().item(), mean=m.mean().item(), std=m.std().item())
+metrics = dict(max=m.max().item(), idx_max=m.argmax().item(),
+               min=m.min().item(), idx_min=m.argmin().item(),
+               mean=m.mean().item(), std=m.std().item())
 print(metrics)
 with open('../resources/metrics_alex_'+model+'.txt', 'w') as f:
     f.write(json.dumps(metrics))
---
+++ src/metrics_PSNR_SSIM.py
@@ -0,0 +1,105 @@
+import numpy as np
+import json
+import pickle
+import os
+from os import listdir
+#import torch
+from os.path import isfile, join
+import tensorflow as tf
+#from utils_alexnet import *
+
+model = 'chromagan'   # su, dahl, zhang, siggraph
+
+'''
+if model == 'original':
+    path = '../img/original/test/'
+else:
+    path = '../img/colorized/'+model+'/test/'
+
+onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+data_metrics = np.empty((len(onlyfiles), 256, 256, 3))
+i = 0
+for files in onlyfiles:
+    img_array = load_img(join(path, files))
+    img_resized = resize_img(img_array, HW=(256, 256), resample=3)
+    data_metrics[i, :, :, :] = img_resized
+    print(i)
+    i += 1
+
+
+np.save('../resources/data_metrics_'+model+'_0_255', data_metrics)
+'''
+'''
+originals = np.load('../resources/data_metrics_original_0_255.npy')
+colorized = np.load('../resources/data_metrics_'+model+'_0_255.npy')
+
+batch = 22   # range in [0, 22]
+ssim = []
+psnr = []
+
+if batch == 22:
+    for img in range(100 * batch, originals.shape[0]):
+        ssim.append(tf.keras.backend.eval(tf.image.ssim(tf.convert_to_tensor(originals[img, :, :, :], dtype=tf.float32),
+                                                        tf.convert_to_tensor(colorized[img, :, :, :], dtype=tf.float32),
+                                                        max_val=255)))
+
+        psnr.append(tf.keras.backend.eval(tf.image.psnr(tf.convert_to_tensor(originals[img, :, :, :], dtype=tf.float32),
+                                                        tf.convert_to_tensor(colorized[img, :, :, :], dtype=tf.float32),
+                                                        max_val=255)))
+        print(img)
+else:
+    for img in range(100*batch, 100*(batch+1)):
+        ssim.append(tf.keras.backend.eval(tf.image.ssim(tf.convert_to_tensor(originals[img,:,:,:], dtype=tf.float32),
+                                                           tf.convert_to_tensor(colorized[img,:,:,:], dtype=tf.float32),
+                                                           max_val=255)))
+
+        psnr.append(tf.keras.backend.eval(tf.image.psnr(tf.convert_to_tensor(originals[img,:,:,:], dtype=tf.float32),
+                                                           tf.convert_to_tensor(colorized[img,:,:,:], dtype=tf.float32),
+                                                           max_val=255)))
+        print(img)
+
+
+if batch != 0:
+    with open('../resources/metrics_batches/metrics_ssim_' + model + '_batch_' + str(batch-1) + '.txt', 'rb') as fp:
+        btc = pickle.load(fp)
+    ssim = btc + ssim
+
+    with open('../resources/metrics_batches/metrics_psnr_' + model + '_batch_' + str(batch-1) + '.txt', 'rb') as fp:
+        btc = pickle.load(fp)
+    psnr = btc + psnr
+
+    os.remove('../resources/metrics_batches/metrics_ssim_' + model + '_batch_' + str(batch - 1) + '.txt')
+    os.remove('../resources/metrics_batches/metrics_psnr_' + model + '_batch_' + str(batch - 1) + '.txt')
+
+with open('../resources/metrics_batches/metrics_ssim_' + model + '_batch_' + str(batch) + '.txt', 'wb') as fp:
+    pickle.dump(ssim, fp)
+with open('../resources/metrics_batches/metrics_psnr_' + model + '_batch_' + str(batch) + '.txt', 'wb') as fp:
+    pickle.dump(psnr, fp)
+
+
+'''
+# Compute statistics:
+with open('../resources/metrics_batches/metrics_ssim_' + model + '_batch_' + str(22) + '.txt', 'rb') as fp:
+    ssim = pickle.load(fp)
+mean_ssim = sum(ssim)/len(ssim)
+std_ssim = np.sqrt(sum([((x - mean_ssim) ** 2) for x in ssim]) / len(ssim))
+metrics_ssim = dict(max=max(ssim), idx_max=ssim.index(max(ssim)),
+                    min=min(ssim), idx_min=ssim.index(min(ssim)),
+                    mean=mean_ssim, std=std_ssim)
+print(metrics_ssim)
+
+with open('../resources/metrics_batches/metrics_psnr_' + model + '_batch_' + str(22) + '.txt', 'rb') as fp:
+    psnr = pickle.load(fp)
+mean_psnr = sum(psnr)/len(psnr)
+std_psnr = np.sqrt(sum([((x - mean_psnr) ** 2) for x in psnr]) / len(psnr))
+metrics_psnr = dict(max=max(psnr), idx_max=psnr.index(max(psnr)),
+                    min=min(psnr), idx_min=psnr.index(min(psnr)),
+                    mean=mean_psnr, std=std_psnr)
+print(metrics_psnr)
+
+with open('../resources/metrics_ssim_'+model+'.txt', 'w') as f:
+    f.write(json.dumps(str(metrics_ssim)))
+
+with open('../resources/metrics_psnr_'+model+'.txt', 'w') as f:
+    f.write(json.dumps(str(metrics_psnr)))
